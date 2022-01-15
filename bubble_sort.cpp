bb4ff8b4ee73631039edfdb82becf96391f8a1d6
harshit2432 <77371572+harshit2432@users.noreply.github.com>
2022-01-15 17:11:37 +0530

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }    
+
+    int i,j;
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
